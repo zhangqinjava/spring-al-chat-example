dc73db21e326778721c88e81773dd24063c93117
zhangqin <zhangqin@umpay.com>
2025-08-06 10:16:41 +0800

    新增流式处理，以及al互动界面

---
+++ spring-chat/README.md
@@ -0,0 +1,10 @@
+项目介绍：Spring Al的聊天模型的实现demo
+环境准备：JDK17+ 、idea2024及以上版本、 Maven 3.6+ 、Spring Boot 3.3.4 、Spring AI 1.0.0
+您需要使用 DeepSeek 创建 API 密钥才能访问 DeepSeek 语言模型。
+依赖：
+spring-boot-starter-web: For creating RESTful web services
+spring-ai-starter-model-deepseek : To integrate Spring AI with OpenAI
+spring-boot-starter-test: For testing purposes
+申请api-key的操作：
+在DeepSeek 开放平台页面创建一个帐户，并在API Keys 页面生成一个令牌。
+
--- spring-chat/src/main/java/org/al/springchat/controller/ChatController.java
+++ spring-chat/src/main/java/org/al/springchat/controller/ChatController.java
@@ -1,9 +1,11 @@
 package org.al.springchat.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +27,65 @@ public class ChatController {
         return Map.of("generation",chatModel.call(message));
     }
 
+    /**
+     * 流式写法
+     * @param message
+     * @return
+     */
     @GetMapping("/ai/generateStream")
     public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         var prompt = new Prompt(new UserMessage(message));
         return chatModel.stream(prompt);
     }
+    @GetMapping("/ai/runtimeOptions")
+    public String runtimeOptions(
+            @RequestParam(value = "message",defaultValue = "你是谁？") String message,
+            @RequestParam(value = "temp" ,required = false) Double temp){
 
+        System.out.println("收到消息："+message+",temp = "+temp);
+
+        Prompt prompt ;
+        if(temp !=null){
+            System.out.println("使用传入的temp...");
+            DeepSeekChatOptions build = DeepSeekChatOptions.builder()
+                    .temperature(temp)
+                    .build();
+
+            prompt = new Prompt(message, build);
+        }else{
+            System.out.println("使用默认的 temp...");
+            prompt = new Prompt(message);
+        }
+
+        ChatResponse resp = chatModel.call(prompt);
+
+        return resp.getResult().getOutput().getText();
+
+    }
+
+    /**
+     * 渐进式流式输出
+     * @param message
+     * @param response
+     * @return
+     */
+    @GetMapping("/ai/generateStream2")
+    public Flux<String> generateStream2(
+            @RequestParam(value = "message",defaultValue = "你是谁？") String message,
+            HttpServletResponse response
+    ) {
+        //设置字符为utf-8编码，避免乱码
+        response.setCharacterEncoding("UTF-8");
+        System.out.println("message = "+message);
+        //与模型对话，流式返回内容
+        Prompt prompt = new Prompt(new UserMessage(message));
+        Flux<ChatResponse> stream = chatModel.stream(prompt);
+
+        Flux<String> resp = stream.map(
+                chatResponse -> chatResponse.getResult().getOutput().getText()
+        );
+        System.out.println("返回内容："+resp);
+        return resp;
+
+    }
 }
